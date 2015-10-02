727fdd621fc361e0bd6f0c949c8753c31edcd566
LightOfFreedom <Light_of_freedom@list.ru>
2015-10-02 21:52:53 +0800

    first commit from home

---
+++ CodeMagick/js/form.js
@@ -0,0 +1,32 @@
+(function() {
+  var formContainer = document.querySelector('.overlay-container');
+  var formOpenButton = document.querySelector('.reviews-controls-new');
+  var formCloseButton = document.querySelector('.review-form-close');
+
+  formOpenButton.onclick = function(evt) {
+    evt.preventDefault();
+    formContainer.classList.remove('invisible');
+  };
+
+  formCloseButton.onclick = function(evt) {
+    evt.preventDefault();
+    formContainer.classList.add('invisible');
+  };
+  var elementName=document.querySelector('.review-form-field-name');
+  elementName.setAttribute("required", true);
+  var elementText=document.querySelector('.review-form-field-text');
+  elementText.setAttribute("required",true);
+
+  document.forms[1].onsubmit = function(evt) {
+    evt.preventDefault();
+    var iChecked=document.forms[1]['review-mark'].value;
+    var nowDate= new Date;
+    var myDate= new Date(1988, 0, 09);
+    var ResultDay=+nowDate+(+nowDate-myDate)/(24*60*60*100);
+
+    document.cookie="checked="+iChecked + "; expires="+ResultDay;
+    //document.cookie="UserName="+document.querySelector('.review-form-field-name').value; expires=ResultDay.toUTCString();
+    document.forms[1].submit();
+  }
+
+})();
