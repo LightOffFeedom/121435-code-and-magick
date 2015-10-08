233c0f46f8bf9ce064c9e33a667f7fb6ed2da3a6
LightOfFreedom <Light_of_freedom@list.ru>
2015-10-08 14:12:17 +0800

    add (function{})()

--- js/reviews.js
+++ js/reviews.js
@@ -1,29 +1,29 @@
 /**
  * Created by odin on 06.10.15.
  */
-
+(function{
     var starsClassName = {
-        '1':'review-rating-one',
-        '2':'review-rating-two',
-        '3':'review-rating-three',
-        '4':'review-rating-four',
-        '5':'review-rating-five',
+        '1': 'review-rating-one',
+        '2': 'review-rating-two',
+        '3': 'review-rating-three',
+        '4': 'review-rating-four',
+        '5': 'review-rating-five',
     };
 
     document.querySelector('.reviews-filter').classList.add('invisible');
 
-    var oTemplate=document.getElementById('review-template');
-    var oReviewsContainer=document.querySelector('.reviews-list');
-    var reviewFragment= document.createDocumentFragment();
+    var oTemplate = document.getElementById('review-template');
+    var oReviewsContainer = document.querySelector('.reviews-list');
+    var reviewFragment = document.createDocumentFragment();
 
-    reviews.forEach(function(review,i){
-        var newReview= oTemplate.content.children[0].cloneNode(true);
-        newReview.querySelector('img').src=review.author.picture;
-        newReview.querySelector('img').alt=review.author.name;
-        newReview.querySelector('img').title=review.author.name;
+    reviews.forEach(function (review, i) {
+        var newReview = oTemplate.content.children[0].cloneNode(true);
+        newReview.querySelector('img').src = review.author.picture;
+        newReview.querySelector('img').alt = review.author.name;
+        newReview.querySelector('img').title = review.author.name;
         //newReview.querySelector('.review-rating').textContent=review.rating;
         newReview.querySelector('.review-rating').classList.add(starsClassName[review['rating']]);
-        newReview.querySelector('.review-text').textContent=review['description'];
+        newReview.querySelector('.review-text').textContent = review['description'];
 
         reviewFragment.appendChild(newReview);
         if (review.author.picture) {
@@ -33,8 +33,8 @@
             reviewBackground.onload = function () {
                 newReview.style.backgroundImage = review.author.picture;
                 newReview.style.backgroundSize = '100% auto';
-                var sp1=reviewBackground;
-                var sp2=oTemplate.querySelector('img');
+                var sp1 = reviewBackground;
+                var sp2 = oTemplate.querySelector('img');
                 oTemplate.replaceChild(sp1, sp2);
 
             }
@@ -49,6 +49,6 @@
 
     oReviewsContainer.appendChild(reviewFragment);
 
-
+})()
 
 
